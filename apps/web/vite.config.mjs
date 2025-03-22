86c3224d1c35522cb75f8241e949c61fc25961bf
sonnv1912 <sonnv1912@gmail.com>
2025-03-22 20:28:27 +0700

    feat: initialize web application structure with basic components and configurations

---
+++ apps/web/vite.config.mjs
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'node:path';
+import tailwindcss from '@tailwindcss/vite';
+
+// https://vite.dev/config/
+export default defineConfig({
+   resolve: {
+      alias: {
+         '@': path.resolve(__dirname, './src'),
+         '@components': path.resolve(__dirname, './src/components'),
+         '@assets': path.resolve(__dirname, './src/assets'),
+         '@hooks': path.resolve(__dirname, './src/hooks'),
+         '@configs': path.resolve(__dirname, './src/configs'),
+         '@layouts': path.resolve(__dirname, './src/layouts'),
+      },
+   },
+   plugins: [react(), tailwindcss()],
+});
